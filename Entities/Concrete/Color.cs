9cabc8fa2cd60eb77ad2e61a58321db14c858b5c
Erdal Yesilyurt <erdyes@outlook.com>
2021-02-28 21:01:42 +0100

    7. Gün - 30 Ocak 2021 Saat 21.00  ödev 2

--- Entities/Concrete/Color.cs
+++ Entities/Concrete/Color.cs
@@ -7,7 +7,7 @@ namespace Entities.Concrete
 {
     public class Color: IEntity
     {
-        public int Colorid { get; set; }
+        public int ColorId { get; set; }
         public int ColorName { get; set; }
     }
 }
